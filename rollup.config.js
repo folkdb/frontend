8564e869533d6b9da8ace1dfbc0ee2429f82d0a7
Daniel Narey <danielnarey@users.noreply.github.com>
2020-07-02 17:45:36 -0400

    sync

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+
+export default [
+  {
+    input: 'src/functions/fetch-data.js',
+    output: [
+      { file: 'lambda/fetch-data.js', format: 'cjs' },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+    ],
+  },
+];
