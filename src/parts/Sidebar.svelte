d8b6b60adbc144a915c9787c7acceb1f49aa4fb9
Daniel Narey <danielnarey@users.noreply.github.com>
2020-07-11 22:15:41 -0400

    sync

--- src/parts/Sidebar.svelte
+++ src/parts/Sidebar.svelte
@@ -1,22 +1,24 @@
 <template lang="pug">
   nav
     p
+      | The FolkDB database is under development. See below for a sample of
+      | songs available to view.
   
 </template>
 
 <style lang="postcss">
   nav
+    @apply z-10
     @apply w-full
-    @apply border-medium-orange
-    @apply bg-light-orange
-
+    @apply border-medium-dark-orange
+    @apply bg-soft-orange
+  
   @screen c11
     nav
       @apply w-4c
       @apply fixed h-screen overflow-y-scroll
-      @apply 
       @apply border-r
-
+  
   @screen c21
     nav
       @apply w-5c
