d7bc87666c133c33cd22cbb71d2c3de6137da685
Daniel Narey <danielnarey@users.noreply.github.com>
2020-07-11 22:06:58 -0400

    sync

--- src/index.svelte
+++ src/index.svelte
@@ -1,6 +1,7 @@
 <script>
   import './styles/index.js';
   import Header from './parts/Header.svelte';
+  import Sidebar from './parts/Sidebar.svelte';
   import Main from './parts/Main.svelte';
   import Footer from './parts/Footer.svelte';
 
@@ -11,7 +12,7 @@
   #app
     Header
 
-    #sidebar
+    Sidebar
       
     Main
     
@@ -26,20 +27,5 @@
   #app
     @apply bg-near-white
     @apply text-darker font-ibm-plex-sans font-regular text-sm
-  
-  #sidebar
-    @apply w-full
-    @apply border-medium-orange
-    @apply bg-light-orange
-
-  @screen c11
-    #sidebar
-      @apply w-4c
-      @apply fixed h-screen overflow-y-scroll
-      @apply border-r
-
-  @screen c21
-    #sidebar
-      @apply w-5c
 
 </style>
---
+++ src/parts/Sidebar.svelte
@@ -0,0 +1,24 @@
+<template lang="pug">
+  nav
+    p
+  
+</template>
+
+<style lang="postcss">
+  nav
+    @apply w-full
+    @apply border-medium-orange
+    @apply bg-light-orange
+
+  @screen c11
+    nav
+      @apply w-4c
+      @apply fixed h-screen overflow-y-scroll
+      @apply 
+      @apply border-r
+
+  @screen c21
+    nav
+      @apply w-5c
+    
+</style>
