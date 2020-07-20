c75e950aba401f56b2c5657078b9ade36cf03d45
Daniel Narey <danielnarey@users.noreply.github.com>
2020-07-20 00:51:25 -0400

    sync

--- src/routes/arrangement/Arrangement.svelte
+++ src/routes/arrangement/Arrangement.svelte
@@ -10,41 +10,38 @@
   export let index;
   
   let data;
+  let arrangement;
   let error;
   
   $: (async () => {
     ({ data, error } = await fetchSong(slug));
-  })();
-  
-  $: arrangement = (
-    data
-      ? (data.arrangements || [])[parseInt(index, 10)]
-      : false
-  );
-  
-  $: content = arrangement ? arrangement.content : false;
-  
-  $: if (content) {
-    if (get(vextabReady)) {
-      renderSvg(content);
-    } else {
-      vextabReady.subscribe((isReady) => {
-        if (isReady) { renderSvg(content); }
-      });
+    
+    const i = parseInt(index, 10);
+    arrangement = data ? (data.arrangements || [])[i] : false;
+    content = arrangement ? arrangement.content : false;
+    
+    if (content) {
+      if (get(vextabReady)) {
+        renderSvg(content);
+      } else {
+        vextabReady.subscribe((isReady) => {
+          if (isReady) { renderSvg(content); }
+        });
+      }
     }
-  }
-  
-  $: loadError = (
-    data && !arrangement
-      ? `You may have arrived here via a broken link or bad URL. There is no arrangement for "${slug}" at index ${index}.`
-      : error
-  );
-  
-  $: parseError = (
-    arrangement && !content
-      ? 'Oops, something is missing! This arrangement has no notation to parse.'
-      : false
-  );
+    
+    loadError = (
+      data && !arrangement
+        ? `You may have arrived here via a broken link or bad URL. There is no arrangement for "${slug}" at index ${index}.`
+        : error
+    );
+    
+    parseError = (
+      arrangement && !content
+        ? 'Oops, something is missing! This arrangement has no notation to parse.'
+        : false
+    );
+  })();
   
   beforeUpdate(() => {
     window.scrollTo({ top: 0 });
