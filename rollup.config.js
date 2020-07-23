dd6123683cfaf62e6c89820d46434848f3736536
danielnarey <accounts@danielnarey.com>
2020-07-23 15:55:59 +0000

    automated sync

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default [
